eb0c5901a1153c955928e005cf88d4767eb41db9
Dmitrii Frolov <frolovdmn@gmail.com>
2024-06-09 11:29:43 +0300

    corrected description

--- rag_conversation/chain.py
+++ rag_conversation/chain.py
@@ -1,6 +1,22 @@
 from langchain_community.llms import Ollama
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 
 model = Ollama(model = 'qwen2')
 embeddings = HuggingFaceEmbeddings()
 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            'system',
+            'description'
+        ),
+        MessagesPlaceholder('chat_history'),
+        (
+            'human', 
+            '{text}'
+        )
+    ]
+)
+
+chain = prompt | model
